2c91acc4783000e043581aee640ebcc62e8ef379
William <williamp0403@gmail.com>
2025-09-01 21:46:43 -0400

    set environment variables

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,9 @@ import cookieParser from 'cookie-parser';
 import RouteLink from './routes/link.route.js'
 import RouteAuth from './routes/auth.route.js'
 
-process.loadEnvFile()
+if (process.env.NODE_ENV !== 'production') {
+  process.loadEnvFile()
+}
 
 const app = express()
 const PORT = process.env.PORT ?? 3003
@@ -14,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
   credentials: true
 }))
 
